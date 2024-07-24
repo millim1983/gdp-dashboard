901c069079e1cb02b08e59cc86cccec459a169c9
millim1983 <kiakiakia83@gmail.com>
2024-07-24 12:47:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
@@ -13,7 +15,9 @@ st.set_page_config(
 # Declare some useful functions.
 
 @st.cache_data
-def get_gdp_data():
+
+def get_data():
+    # 원본데이터를 불러와 전처리하는 과정을 넣을 것 
     """Grab GDP data from a CSV file.
 
     This uses caching to avoid having to read the file every time. If we were
@@ -22,42 +26,60 @@ def get_gdp_data():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    location = Path(__file__).parent/'data/steel_ai_01_on.csv'
+    raw_df = pd.read_csv(location, header = 0)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    # MIN_YEAR = 1960
+    # MAX_YEAR = 2022
 
     # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
+    # FACTORY	공장코드
+    # WORK_SHAPE	작업조(1근/2근/3근)
+    # INPUT_ED	투입되는 소재의 외경(mm)
+    # INPUT_LENGTH	투입되는 소재의 개별 길이(mm)
+    # INPUT_QTY	투입 총 수량(개)
+    # DIRECTION_ED	가공 후 목표 외경(mm)
+    # OUTPUT_ED	생산된 소재의 외경(mm)
+    # STEEL_CATEGORY	투입 강종 분류
+    # WORK_START_DT	작업시작일시
+    # WORK_END_DT	작업종료일시
     #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+    # 종속변수
+    # diff = WORK_END_DT -  WORK_START_DT
+    # INPUT_QTY
+    # OUTPUT_ED
+    # 투입되는 소재 총길이 = 
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+    # 데이터 타입 변경
 
-    return gdp_df
+    raw_df = raw_df.astype({'STEEL_CATEGORY' : 'category'})  # STEEL_CATEGORY 값이 6개이므로 타입을 category로 변경
+    raw_df = raw_df.astype({'WORK_SHAPE':'object'})  # WORK_SHAPE은 숫자이지만, 작업조를 나타냄 1근, 2근 3근 > object로 변경
 
-gdp_df = get_gdp_data()
+    # 중복데이터 제거 
+
+    raw_df = raw_df.drop_duplicates()
+
+    # 종속변수 추가 
+    raw_dff['work_start_dt_ns'] = pd.to_datetime(raw_df['WORK_START_DT'])
+    raw_df['work_end_dt_ns'] = pd.to_datetime(raw_df['WORK_END_DT'])
+
+    raw_df['diff'] = (raw_df['work_end_dt_ns'] - raw_df['work_start_dt_ns']).dt.total_seconds().div(60).astype(int)
+
+    # # 원본의 year 별 gdp 를 year vs gdp 로 변환하는 과정 
+    # # So let's pivot all those year-columns into two: Year and GDP
+    # gdp_df = raw_gdp_df.melt(
+    #     ['Country Code'],
+    #     [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
+    #     'Year',
+    #     'GDP',
+    # )
+    # # Convert years from string to integers
+    # gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+
+
+    return df
+
+df = get_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
@@ -75,16 +97,71 @@ But it's otherwise a great (and did I mention _free_?) source of data.
 ''
 ''
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+# 그래프를 그리기 위해 int, float만 선택 
+df1 = df.select_dtypes([int, float])
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
+# 데이터 describ을 표현하기 위한 섹션 
+cols = st.columns(4)    # 4열로보여주겠다 
+
+
+for i, d in enumerate(selected_countries):
+    col = cols[i % len(cols)]
+
+    with col:
+        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+
+        if math.isnan(first_gdp):
+            growth = 'n/a'
+            delta_color = 'off'
+        else:
+            growth = f'{last_gdp / first_gdp:,.2f}x'
+            delta_color = 'normal'
+
+        st.metric(
+            label=f'{country} GDP',
+            value=f'{last_gdp:,.0f}B',
+            delta=growth,
+            delta_color=delta_color
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+min_value = 10
+max_value = 100
+
+from_bins, to_bins = st.slider(
+    '히스토그램 bins(데이터범위)에 따른 그래프를 살펴보세요',
     min_value=min_value,
     max_value=max_value,
     value=[min_value, max_value])
 
-countries = gdp_df['Country Code'].unique()
+vars = df.columns
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if not len(countries):
     st.warning("Select at least one country")
