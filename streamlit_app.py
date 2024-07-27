965ff0949b552443443b1b3da47fad110b6df68e
millim1983 <kiakiakia83@gmail.com>
2024-07-28 00:05:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,31 @@ import math
 from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns 
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
+import plotly.express as px
+import io
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='황삭 가공 제조 데이터 분석',
     page_icon=':factory:', # This is an emoji shortcode. Could be a URL too.
 )
+
+# ----------- 사이드 바 메뉴 ---------- # 
+with st.sidebar:
+    choose = option_menu("빅분기프로젝트", ["About", "PROJECT", "DATA"],
+    icons=['house', 'camera fill', 'kanban'],
+    menu_icon="app-indicator", default_index=0,
+    styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
+
+
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
@@ -65,6 +84,7 @@ def get_data():
 
     raw_df['diff'] = (raw_df['work_end_dt_ns'] - raw_df['work_start_dt_ns']).dt.total_seconds().div(60).astype(int)
 
+
     # # 원본의 year 별 gdp 를 year vs gdp 로 변환하는 과정 
     # # So let's pivot all those year-columns into two: Year and GDP
     # gdp_df = raw_gdp_df.melt(
@@ -82,31 +102,58 @@ def get_data():
 df = get_data()
 # 그래프를 그리기 위해 int, float만 선택 
 df1 = df.select_dtypes([int, float])
+df1_col = df1.columns
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+
+# ----------- 본문 ------------------ # 
+
 
 # Set the title that appears at the top of the page.
 st.header(':factory: 제조데이터 분석 프로젝트')
-'''
-
-
-빅데이터 분석의 첫걸음! 
-제조데이터 분석을 위한 데이터 시각화 표출 대시보드입니다. 
-다양한 그래프들을 활용하여 데이터 특성을 시각적으로 나타냈습니다.
+st.subheader('빅데이터 분석의 첫걸음! ')
+st.markdown("""
+제조데이터 분석을 위한 데이터 시각화 표출 대시보드입니다.   
+다양한 그래프들을 활용하여 데이터 특성을 시각적으로 나타냈습니다.  
 변수들을 조정하며 데이터의 특성을 찾아봅시다. 
-
-'''
+""")
+# 줄바꿈시 띄어쓰기 두개 
 
 # Add some spacing
 ''
 ''
+# ---- 슬라이드 바로 bins 조정 --- # 
+min_value = df1['diff'].min()
+max_value = df1['diff'].max()
+from_year, to_year = st.slider(
+    '작업시간 범위를 정해보세요?',
+    min_value=min_value,
+    max_value=max_value,
+    value=[min_value, max_value])
+
+#----- int, float 형의 데이터 describe 보여주기 --- # 
+
+steel_cat = df1['STEEL_CATEGORY'].unique()
 
 
+if not len(df1_col):
+    st.warning("적어도 한개 이상의 컬럼을 선택하세요?")
+
+selected_col = st.multiselect(
+    '컬럼을 선택하세요!',  
+    #columns,  
+    ['WORK_SHAPE', 'INPUT_ED', 'INPUT_LENGTH', 'INPUT_QTY', 'DIRECTION_ED','OUTPUT_ED', 'STEEL_CATEGORY'])
+
+
+'''
 # 데이터 describe을 표현하기 위한 섹션 
 cols = st.columns(4)    # 4열로보여주겠다 
+#col_selection = 
+for i in col :
+    description = df1[i].describe()
+    for k, v in enumerate(desc):
+        print(i, k,v)
 
-for i, d in enumerate(selected_countries):
+for i, d in enumerate(df.describe()):
     col = cols[i % len(cols)]
 
     with col:
@@ -138,7 +185,6 @@ from_bins, to_bins = st.slider(
     value=[min_value, max_value])
 
 vars = df.columns
-
 
 
 
@@ -218,3 +264,4 @@ for i, country in enumerate(selected_countries):
             delta=growth,
             delta_color=delta_color
         )
+'''
